3d30eefdcb7a45d8233c9e92891a3b2e36512a1f
Andy Longhurst <andy@longhurst.net>
2016-05-13 14:26:00 +0100

    Removed unused imports

---
+++ Assets/Scripts/SimulationSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    public class SimulationSettings
+    {
+        [SerializeField]
+        public string ExperimentName { get; set; }
+
+        // Sections for colony
+        // Sections for ant behaviour etc
+        // Sections for map
+
+        public SimulationSettings()
+        {
+            ExperimentName = "Experiment1";
+        }
+    }
+}
