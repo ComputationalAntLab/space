bbe5198e1507f9e588221667424543bb02a94bc8
Andy Longhurst <andy@longhurst.net>
2016-06-13 15:51:09 +0100

    Refactored pheromone

--- Assets/Scripts/Pheromone.cs
+++ Assets/Scripts/Pheromone.cs
@@ -1,90 +1,129 @@
+using System;
+using Assets.Scripts;
 using UnityEngine;
 
-public class Pheromone : MonoBehaviour 
+public class Pheromone : MonoBehaviour, ITickable
 {
-	public float strength;				//strength of pheromone between 0 and 1
-	public float duration; 				//total duration of pheromone in seconds, pheromone weakens linearly from 1 to 0 over duration
-	public AntMovement owner; 			//the ant who laid this pheromone 
-	public bool assessingPheromone;		//identifies if the pheromone is for assessing
-	public bool assessingPheromoneCounted;
-	
-	
-	//greg edit
-	public void LayScouting(AntMovement owner) {
-		SphereCollider myCollider = transform.GetComponent<SphereCollider>();
-		//myCollider.radius = 0.5f;
-		myCollider.radius = 2f;
-		
-		this.owner = owner;
+    public float strength;              //strength of pheromone between 0 and 1
+    public float duration;              //total duration of pheromone in seconds, pheromone weakens linearly from 1 to 0 over duration
+    public AntMovement owner;           //the ant who laid this pheromone 
+    public bool assessingPheromone;     //identifies if the pheromone is for assessing
+    public bool assessingPheromoneCounted;
+
+    private AntManager.BehaviourState _behaviour;
+
+    public bool ShouldBeRemoved { get; private set; }
+
+    float _elapsed = 0;
+    public void Tick(float elapsedSimulationMS)
+    {
+        // This tick simulates the behaviour of a tick every second from InvokeRepeating
+        if (Ticker.Should(elapsedSimulationMS, ref _elapsed, 1000))
+        {
+            switch (_behaviour)
+            {
+                case AntManager.BehaviourState.Assessing:
+                    UpdateAssessing();
+                    break;
+                case AntManager.BehaviourState.Recruiting:
+                case AntManager.BehaviourState.ReversingLeading:
+                    UpdateTandeom();
+                    break;
+                case AntManager.BehaviourState.Scouting:
+                    UpdateScouting();
+                    break;
+                default:
+                    DestroyPheromone();
+                    break;
+            }
+        }
+    }
+
+    //greg edit
+    public void LayScouting(AntMovement owner)
+    {
+        _behaviour = AntManager.BehaviourState.Scouting;
+
+        SphereCollider myCollider = transform.GetComponent<SphereCollider>();
+        //myCollider.radius = 0.5f;
+        myCollider.radius = 2f;
+
+        this.owner = owner;
         strength = -1.0f;
         assessingPheromone = false;
-		
-//		InvokeRepeating("WeakenScouting", 1, 1);
-	}
-	//greg edit
-	private void WeakenScouting() {
-        strength += (0.5f/duration);
-		if(strength >= 0)
-			Destroy(gameObject);
-	}
+    }
 
-	
-	public void LayTandem(AntMovement owner) {
+    //greg edit
+    private void UpdateScouting()
+    {
+        strength += (0.5f / duration);
+        if (strength >= 0)
+            DestroyPheromone();
+    }
 
-		SphereCollider myCollider = transform.GetComponent<SphereCollider>();
-		myCollider.radius = 0.5f;
-		
-		this.owner = owner;
-		
-		if (this.owner.ant.state == AntManager.BehaviourState.Recruiting) {
+    public void LayTandem(AntMovement owner)
+    {
+        SphereCollider myCollider = transform.GetComponent<SphereCollider>();
+        myCollider.radius = 0.5f;
+
+        this.owner = owner;
+
+        if (this.owner.ant.state == AntManager.BehaviourState.Recruiting)
+        {
             strength = 1.05f;
-		} else {
+            _behaviour = AntManager.BehaviourState.Recruiting;
+        }
+        else
+        {
+            _behaviour = AntManager.BehaviourState.ReversingLeading;
             strength = 1.0f;
-		}
+        }
 
-        assessingPheromone = false;		
-		InvokeRepeating("Weaken", 1, 1);
-	}
-	
-	private void Weaken() {
-        strength -= (0.5f/duration);
-		if(strength <= 0)
-			Destroy(gameObject);
-	}
-	
-	
-	public void LayAssessing(AntMovement owner) {
-		SphereCollider myCollider = transform.GetComponent<SphereCollider>();
-		myCollider.radius = 0.125f; 
-		
-		this.owner = owner;
+        assessingPheromone = false;
+        InvokeRepeating("Weaken", 1, 1);
+    }
+
+    private void UpdateTandeom()
+    {
+        strength -= (0.5f / duration);
+        if (strength <= 0)
+            DestroyPheromone();
+    }
+    
+    public void LayAssessing(AntMovement owner)
+    {
+        _behaviour = AntManager.BehaviourState.Assessing;
+
+        SphereCollider myCollider = transform.GetComponent<SphereCollider>();
+        myCollider.radius = 0.125f;
+
+        this.owner = owner;
         strength = 0.0f;
         assessingPheromone = true;
         assessingPheromoneCounted = false;
-		
-		// when the owner is no longer assessing the nest remove the pheromones
-		InvokeRepeating("Remove", 1, 1);
-		InvokeRepeating("ResetPheromoneCount", 1, 1);
-	}
-	
-	private void Remove() {
-		if(owner.ant.state != AntManager.BehaviourState.Assessing) {
-			Destroy(gameObject);
-		}
-	}
-	
-	private void ResetPheromoneCount() {
-		if (assessingPheromoneCounted == true) {
-			if (Vector3.Distance (transform.position, owner.transform.position) > (owner.assessmentPheromoneRange * 1.1f) ) {
+    }
+
+    private void UpdateAssessing()
+    {
+        if (owner.ant.state != AntManager.BehaviourState.Assessing)
+        {
+            DestroyPheromone();
+        }
+
+        if (assessingPheromoneCounted == true)
+        {
+            if (Vector3.Distance(transform.position, owner.transform.position) > (owner.assessmentPheromoneRange * 1.1f))
+            {
                 assessingPheromoneCounted = false;
-			}
-		}
-	}
-	
-	public void destroyPheromone() {
-		Destroy(gameObject);
-	}
-	
+            }
+        }
+    }
+
+    public void DestroyPheromone()
+    {
+        Destroy(gameObject);
+        ShouldBeRemoved = true;
+    }
 }
 
 /*
---
+++ Assets/Scripts/TickHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    public static class Ticker
+    {
+        public static bool Should(float elapsed, ref float runningTotal, float target)
+        {
+            runningTotal += elapsed;
+            if(runningTotal >= target)
+            {
+                runningTotal -= target;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
