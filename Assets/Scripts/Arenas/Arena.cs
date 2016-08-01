d5b8140fa0c49fb9926c0733e0232d7de3d7abdc
Andy Longhurst <andy@longhurst.net>
2016-08-01 10:29:45 +0100

    moving files to resources

---
+++ Assets/Scripts/Arenas/Arena.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Arenas
+{
+    [Serializable]
+    public class Arena
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public Nest StartingNest { get; set; }
+
+        public List<Nest> NewNests { get; set; }
+
+        public Arena()
+        {
+            NewNests = new List<Nest>();
+        }
+    }
+}
---
+++ Assets/Scripts/Arenas/Nest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Arenas
+{
+    [Serializable]
+    public class Nest
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int PositionX { get; set; }
+
+        public int PositionY { get; set; }
+
+        public float Quality { get; set; }
+    }
+}
